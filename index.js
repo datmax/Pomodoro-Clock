a935c88420c67ca0875678dbb42d344aad29ff77
Max Datsun <unrusso@live.com>
2017-07-20 19:31:05 +0200

    Added background color change and time displayed on title.

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
 $(document).ready(function(){
+    // have to do this because, to call the clearInterval function, the setInterval needs to be global.
+    var timeInterval;
+    // same for bgInterval.
+    var bgInterval;
+    var colors = [
+        'lightgreen',
+        'lightblue',
+        'lightcoral',
+        'yellowgreen',
+        'grey',
+        'springgreen',
+        'slateblue'
+    ];
 
-    var interval;
     var started = false;
-    paused = false;
+    var paused = false;
     var newTime = null;
     var time = $(".time");
     var initialTimeText = time.text();
     var button = $("button");
     
 
+
+
     intervalUpdate = () =>{
         if(!paused){
         newTime = convertInSeconds();
         newTime -=1;
-        updateTime(newTime);
+        update(newTime);
         }
 
     }
 
+    bgUpdate = (bgs) =>{
+        if(!paused){
+        console.log("hey");
+        $(":root").css("--bgcolor", colors[Math.floor(Math.random() * bgs.length) + 1]);
+        }
+        
+    }
+
     //when user clicks play, converts time in seconds and calls the
-    //updateTime function every second.
+    //update function every second.
     startTimer = () => {
         paused = false;
         if(!started){
-           interval = setInterval(function(){intervalUpdate();}, 1000);
+           timeInterval = setInterval(function(){intervalUpdate();}, 1000);
+           bgInterval = setInterval(function(){bgUpdate(colors);}, 30000);
         }
         started = true;
         
@@ -41,26 +64,22 @@ $(document).ready(function(){
         var valueList = value.split(' ')
         var timeChanging = convertInSeconds();
         timeChanging += valueList[1] === 'm' ? parseInt(valueList[0]) * 60 : parseInt(valueList[0]);
-        updateTime(timeChanging);
+        update(timeChanging);
     }
 
 
     //updates time on screen, converting it back to minutes and seconds and 
-    //formatting it.
-    updateTime = (timeToUpdate) => {
+    //formatting it.Also changes background color.
+    update = (timeToUpdate) => {
         if(timeToUpdate >= 0){
-
             var minutes = Math.floor(timeToUpdate / 60);
             var seconds = timeToUpdate % 60 >= 10 ? timeToUpdate % 60 : '0'+ timeToUpdate % 60;
-            console.log(typeof(seconds));
             time.text(minutes + ':' + seconds);
+            $("title").text(minutes + ':' + seconds);
         }
         
 
     }
-
-
-
 
     button.click(function(){
         switch(this.value){
@@ -71,7 +90,8 @@ $(document).ready(function(){
                 paused = true;
                 break;
             case 'reset':
-                clearInterval(interval);
+                clearInterval(timeInterval);
+                clearInterval(bgInterval);
                 time.text(initialTimeText);
                 started = false;
                 break;
